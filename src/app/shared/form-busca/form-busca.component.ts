42461644f7d4a0a6635997b1f9e1640f7db0aa8a
joneilson pereira <joneilson84@gmail.com>
2024-04-16 16:29:54 -0300

    feat: adicionando modal viajante

--- src/app/shared/form-busca/form-busca.component.ts
+++ src/app/shared/form-busca/form-busca.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-form-busca',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './form-busca.component.scss'
 })
 export class FormBuscaComponent {
+  constructor(public dialog: MatDialog) {}
 
+  openDialog() {
+    // this.dialog.open(DialogElementsExampleDialog);
+    this.dialog.open(ModalComponent);
+  }
 }
