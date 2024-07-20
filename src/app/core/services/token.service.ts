c7aacc0e6fecbc1c225aafe1a8cc17da5d4cf6dc
joneilsonlima <joneilson84@gmail.com>
2024-07-20 18:43:13 -0300

    feat: adicionando a dependência jwt e criando service para token

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  salvarToken(token: string): void {
+    return localStorage.setItem(KEY, token);
+  }
+
+  excluirToken(): void {
+    localStorage.getItem(KEY);
+  }
+
+  retornarToken() {
+    return localStorage.getItem(KEY);
+  }
+
+
+  possuiToken() {
+    return !!this.retornarToken();
+  }
+}
